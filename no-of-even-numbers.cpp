b7c90b8483fd8a8ea3424c8e4373a70cc748dfbd
shahnawaz-alam37 <shahnawazalam.cme@gmail.com>
2022-04-05 11:45:28 -0700

    user specified array

--- no-of-even-numbers.cpp
+++ no-of-even-numbers.cpp
@@ -2,15 +2,19 @@
 using namespace std;
 int main(){
     int count=0;
-    int arr[10]={1,2,3,4,5,6,7,8,9,10};
+    int arr[10];
+    cout<<"enter any 10 numbers:"<<endl;
+    for (int i = 0; i < 10; i++)
+    {
+        cin>>arr[i];
+    }
     for (int i = 0; i < 10; i++)
     {
         if (arr[i]%2==0)
         {
             count++;
-        }
-        
+        }   
     }
-    cout<<count;
+    cout<<"there are "<<count<<" even numbers present";
     return 0;
 }
