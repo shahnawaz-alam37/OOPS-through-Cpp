d1385ed05fb2024990ec7296201e44cb4edd17e2
shahnawaz-alam37 <shahnawazalam.cme@gmail.com>
2022-04-08 07:03:55 -0700

    pointer to variables

---
+++ observation-2/new-delete.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class info{
+    int s;
+    public:
+        int area(int x){
+            s=x;
+            int area=s*s*s;   
+            return(area);
+        }
+};
+
+int main(){
+    info obj;
+    int *p=new int;
+    cout<<"enter the side of squar:";
+    cin>>*p;
+    cout<<"area = "<<obj.area(*p);
+    delete p;
+    return 0;
+}
