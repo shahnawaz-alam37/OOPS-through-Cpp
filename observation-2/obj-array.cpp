26dccd4a86439f3dcbb550d08d73cc6bff0de828
shahnawaz-alam37 <shahnawazalam.cme@gmail.com>
2022-04-08 03:38:26 -0700

    array of objects

---
+++ observation-2/obj-array.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+class objarray{
+    int age;
+    public:
+        void value(int num){
+            age=num;
+        }
+        void disp(){
+            cout<<age<<endl;
+        }
+};
+
+int main(){
+    objarray ob[2];
+    int a,b;
+    cout<<"enter age of fist and sencond student:";
+    cin>>a>>b;
+    ob[1].value(a);
+    ob[2].value(b);
+    cout<<"age of first student=";
+    ob[1].disp();
+    cout<<"age of second student=";
+    ob[2].disp();
+    return 0;
+}
