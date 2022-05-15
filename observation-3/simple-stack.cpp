292d2f2cca212f4239cac3c43446c04555a6f522
shahnawaz-alam37 <shahnawazalam.cme@gmail.com>
2022-05-15 05:07:05 -0700

    simple stack, no issues

--- observation-3/simple-stack.cpp
+++ observation-3/simple-stack.cpp
@@ -21,13 +21,16 @@ int main ()
     Stack<int> int_stack;
     Stack<string> str_stack;
     cout<<"values pushed into the stack"<<endl;
-    int_stack.push(67);
     str_stack.push("world");
     str_stack.push("Hello");
-    
+    str_stack.push("welcome");
+    int_stack.push(10);
+    int_stack.push(20);
     cout<<"value popped from the stack"<<endl;
+    cout << str_stack.pop() << endl;
     cout << str_stack.pop() << endl; 
     cout << str_stack.pop() << endl;
+    cout << int_stack.pop() << endl;
     cout << int_stack.pop() << endl;
     return 0;
 }
